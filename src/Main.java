aa4b69755c14d6fb4eecfd4ea05275b358410a3d
Kirill Markov <isikir668@gmail.com>
2023-04-06 22:07:40 +0300

    DZ

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+        public static void main(String[] args) {
+            // Задание 1.
+            int rubles = 6000;
+            int miles = rubles / 20;
+
+            System.out.println("Количество полученных миль за стоимость билета:");
+            System.out.println(miles);
+
+    }
+}
